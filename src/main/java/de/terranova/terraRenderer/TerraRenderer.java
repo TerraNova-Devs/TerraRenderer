ef054a62e4c2e3df3abfe7f71109ce9c1e1f9d32
VonNekyia <germarten@gmail.com>
2025-11-16 12:38:12 +0100

    initial settings

---
+++ src/main/java/de/terranova/terraRenderer/TerraRenderer.java
@@ -0,0 +1,17 @@
+package de.terranova.terraRenderer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TerraRenderer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
